75f25c5652be4533f202a3751801ea6045a1bfbc
Rahul <arolirahul@gmail.com>
2023-06-05 11:46:47 +0530

    This is my first code push in GIT. jwdujbdj

--- test1.py
+++ test1.py
@@ -2,3 +2,6 @@ from pymongo.mongo_client import MongoClient
 uri = "mongodb+srv://arolirahul:rahul@1998@cluster69.4nbex2j.mongodb.net/?retryWrites=true&w=majority"
 # Create a new client and connect to the server
 client = MongoClient(uri)
+
+
+"""kbvdjbvfjk v kjfvbjkf v j"""
